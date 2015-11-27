3d80a1eb57695a0d189869b043bf20ede4343568
pa001024 <pa001024@qq.com>
2015-11-27 19:49:24 +0800

    update v0.5

---
+++ bookmark.js
@@ -0,0 +1,32 @@
+// BilibiliDownload 书签v0.0.1
+(function(){
+    if(location.hostname != "www.bilibili.com" || !$(".v-title > h1").length) {
+        alert("然而并没有用: \n只能在bilibili视频页面使用哦");
+        return;
+    }
+    function downloadURL(url,filename) {
+        var i = document.createElement("a"), o = document.createEvent("MouseEvent");
+        i.href = url;
+        i.download = filename;
+        i.rel = "noreferrer";
+        o.initEvent("click", !0, !0, window, 1, 0, 0, 0, 0, !1, !1, !1, !1, 0, null);
+        i.dispatchEvent(o);
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://bilibili.download/get.php?callback=?',
+        data: { url: location.href },
+        dataType:"jsonp",
+        success: function(data) {
+            var filename = $(".v-title > h1").attr("title")+" ";
+            filename += location.pathname.substring(7)
+                .replace(/\/|\.html/g,"").replace(/index_(\d+)/g,function(a,b){return "_p"+b});
+            console.log(data.dlink);
+            downloadURL(data.dlink, filename);
+        },
+        error: function() {
+            alert("有麻烦了\n o(` · ~ · ′。)o 也许是网络问题...");
+        }
+    });
+})();
--- get.php
+++ get.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
 $url = $_GET["url"];
+$callback = $_GET["callback"];
 if (strpos($url, "http") !== FALSE) {
 	$after = ereg_replace('com', 'download', $url);
 	echo"<script>{location.href='$after'}</script>";
@@ -34,16 +35,20 @@ else
 fwrite($myfile, $ipaddress."   ".$url."   ".$rurl[0]."\n");
 fclose($myfile);
 
-if (strpos($rurl[0], "http") !== FALSE) {
-	header("Location: $rurl[0]");
-	echo"<script>alert('已经开始下载！');history.go(-1);</script>";  
-	exit;
-} elseif ($rurl[0] == "error2") {
-	echo "<script>{window.alert('网址不正确！');location.href='/'};</script>";
-	exit;
+if ($callback != "") {
+    echo $callback . "($rurl[0])";
+} else {
+    if (strpos($rurl[0], "http") !== FALSE) {
+        header("Location: $rurl[0]");
+        echo"<script>alert('已经开始下载！');history.go(-1);</script>";
+        exit;
+    } elseif ($rurl[0] == "error2") {
+        echo "<script>{window.alert('网址不正确！');location.href='/'};</script>";
+        exit;
+    }
+    echo "<script>{window.alert('出现异常错误，可能为请求过频或视频已经被删除，请稍后再试，抱歉！');location.href='/'};</script>";
+    exit;
 }
-	echo "<script>{window.alert('出现异常错误，可能为请求过频或视频已经被删除，请稍后再试，抱歉！');location.href='/'};</script>";
-	exit;
 ?>
 
 
