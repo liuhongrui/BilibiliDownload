cdc1df5ce477bf5595068f899fa239b13bd68e6b
pa001024 <pa001024@qq.com>
2015-11-29 01:59:43 +0800

    bookmark v0.0.2

--- bookmark.js
+++ bookmark.js
@@ -1,4 +1,4 @@
-// BilibiliDownload 书签v0.0.1
+// BilibiliDownload 书签v0.0.2
 (function(){
     if(location.hostname != "www.bilibili.com" || !$(".v-title > h1").length) {
         alert("然而并没有用: \n只能在bilibili视频页面使用哦");
@@ -13,20 +13,26 @@
         i.dispatchEvent(o);
     }
 
-    $.ajax({
-        type: 'GET',
-        url: 'http://dynamic.bilibili.download/get.php?callback=?',
-        data: { url: location.href },
-        dataType:"jsonp",
-        success: function(url) {
-            var filename = $(".v-title > h1").attr("title")+" ";
-            filename += location.pathname.substring(7)
-                .replace(/\/|\.html/g,"").replace(/index_(\d+)/g,function(a,b){return "_p"+b});
-            console.log(url, filename);
-            downloadURL(url, filename);
-        },
-        error: function() {
-            alert("有麻烦了\n o(` · ~ · ′。)o 也许是网络问题...");
-        }
-    });
+    if(typeof window.bd_fi != "boolean") window.bd_fi = !0;
+    if(bd_fi) {
+        bd_fi = !1;
+        $.ajax({
+            type: 'GET',
+            url: 'http://dynamic.bilibili.download/get.php?callback=?',
+            data: { url: location.origin+location.pathname }, // no hash
+            dataType:"jsonp",
+            success: function(url) {
+                var filename = $(".v-title > h1").attr("title")+" ";
+                filename += location.pathname.substring(7)
+                    .replace(/\/|\.html/g,"").replace(/index_(\d+)/g,function(a,b){return "_p"+b});
+                console.log(url, filename);
+                downloadURL(url, filename);
+                bd_fi = !0;
+            },
+            error: function() {
+                alert("有麻烦了\n o(` · ~ · ′。)o 也许是网络问题...");
+                bd_fi = !0;
+            }
+        });
+    } else alert("别着急, 正在努力解析中");
 })();
